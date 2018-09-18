ee2d2a8d9a7756b9c0a0235d0e572d76f00c7417
Austin Suarez <amsuarez@outlook.com>
2018-09-18 08:37:53 -0700

    can now initialize db

--- Basic Forms Application/app.py
+++ Basic Forms Application/app.py
@@ -1,17 +1,39 @@
 #importing flask
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,g
 import json
 import sqlite3
 
 app = Flask(__name__)
 
 """TODO: 
-       initialize the DB 
+       figure out how to initialize the DB and connect the rest of the time
+       parse json 
+       insert data to json
+       update data
 """
+
+#sets the path of the database
+DATABASE = './data.db'
+
+#connects to the database
+def get_db():
+    db = getattr(g, '_data', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+#initializes the data base using flask
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('init.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 #starting point to the flask app
 @app.route("/")
 def homePage():
+    init_db()
     return render_template('echo.html')
 
 #triggered once the find forms button is pressed
@@ -35,5 +57,4 @@ def setJson(data):
     return
 
 if __name__ == "__main__":
-
     app.run(debug=True)
--- Basic Forms Application/init.sql
+++ Basic Forms Application/init.sql
@@ -1,16 +1,16 @@
-CREATE TABLE USERS {
+CREATE TABLE USERS (
         username varchar(255)  NOT NULL,
         userpass password  NOT NULL
-};
+)
 
 
-CREATE TABLE FORMS{
+CREATE TABLE FORMS()
         id       integer PRIMARY KEY,
         formname varchar(255)  NOT NULL,
         creator  varchar(255)
-};
+)
 
-CREATE [unique] INDEX id on TABLE FORMS(id)
+CREATE [UNIQUE] INDEX id on TABLE FORMS(id)
 
 INSERT INTO USERS (username, userpass)
     VALUES ('tim','password')
