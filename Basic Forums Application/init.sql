5259a0cf7776b6b63db4c68c998c89f8b3f2b5ac
Andrew Nguyen <ahnguyen714@gmail.com>
2018-09-20 13:28:37 -0700

    init.sql updatead to initialize tables and insert data. Everytime the program starts, it drops all table and rebuild to ensure database is clean. We can remove this option later

--- Basic Forums Application/init.sql
+++ Basic Forums Application/init.sql
@@ -1,19 +1,51 @@
-CREATE TABLE USERS (
-        username varchar(255)  NOT NULL,
-        userpass password  NOT NULL
-)
+--Dropping tables to clear data from DB for testing--
+DROP TABLE IF EXISTS forum;
+DROP TABLE IF EXISTS threads;
+DROP TABLE IF EXISTS auth_users;
+DROP TABLE IF EXISTS posts;
 
+PRAGMA foreign_keys = ON;
 
-CREATE TABLE FORUMS()
-        id       integer PRIMARY KEY,
-        formname varchar(255)  NOT NULL,
-        creator  varchar(255)
-)
+CREATE TABLE forums (
+	id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	name TEXT NOT NULL,
+	creator TEXT NOT NULL,
+	FOREIGN KEY(creator) REFERENCES auth_users(username)
+);
 
-CREATE [UNIQUE] INDEX id on TABLE FORUMS(id)
+CREATE TABLE threads (
+	id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	forum_id INT NOT NULL,
+	title TEXT NOT NULL,
+	creator TEXT NOT NULL,
+	time_created TEXT NOT NULL,
+	FOREIGN KEY(creator) REFERENCES auth_users(username),
+	FOREIGN KEY(forum_id) REFERENCES forums(id)
+);
 
-INSERT INTO USERS (username, userpass)
-    VALUES ('tim','password')
+CREATE TABLE posts (
+	thread_id INT NOT NULL,
+	body TEXT NOT NULL,
+	creator TEXT NOT NULL, 
+	created TEXT NOT NULL,
+	FOREIGN KEY(creator) REFERENCES auth_users(username),
+	FOREIGN KEY(thread_id) REFERENCES threads(id)
+);
 
-INSERT INTO USERS (id, formname, creator)
-    VALUES (id,'cassandra', 'tim')
+CREATE TABLE auth_users (
+	username TEXT PRIMARY KEY NOT NULL UNIQUE,
+	password TEXT NOT NULL
+);
+
+--Generating test cases--
+INSERT INTO forums VALUES(1, "redis", "alice");
+INSERT INTO forums VALUES(2, "mongodb", "bob");
+INSERT INTO auth_users VALUES("alice", "Gr3atPA$$W0Rd");
+INSERT INTO auth_users VALUES("bob", "Gr3attPA$$W0Rd");
+INSERT INTO auth_users VALUES("charlie", "Gr3attPA$$W0Rd");
+INSERT INTO threads VALUES(1, 1, "Does anyone know how to start Redis?", "bob", "Wed, 05 Sep 2018 16:22:29 GMT");
+INSERT INTO threads VALUES(2, 1, "Has anyone heard of Edis?", "charlie", "Tue, 04 Sep 2018 13:18:43 GMT");
+INSERT INTO threads VALUES(3, 2, "Ask MongoDB questeions here!", "alice", "Tue, 06 Sep 2018 17:18:43 GMT");
+INSERT INTO posts VALUES(3, "I'm trying to connect to MongoDB, but it doesn't seem to be running.", "bob", "Tue, 04 Sep 2018 15:42:28 GMT");
+INSERT INTO posts VALUES(3, "Ummm. maybe 'sudo service start mongodb'?", "bob", "Tue, 04 Sep 2018 15:45:36 GMT");
+INSERT INTO posts VALUES(2, "It is some new framework for Redis.. disregard..", "charlie", "Tue, 04 Sep 2018 13:49:36 GMT");
